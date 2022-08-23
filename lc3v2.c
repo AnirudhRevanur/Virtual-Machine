c4d9741be50678ef40f0b33acd5eedeb1c9577e7
ClayTsunami <claytsunami@gmail.com>
2022-08-23 19:09:27 +0530

    oof

--- lc3v2.c
+++ lc3v2.c
@@ -6,7 +6,6 @@
 #include <conio.h>  // _kbhit
 
 HANDLE hStdin = INVALID_HANDLE_VALUE;
-
 enum
 {
     R_R0 = 0,
@@ -24,7 +23,7 @@ enum
 enum
 {
     FL_POS = 1 << 0, /* P */
-    FL_ZRO = 1 << 1, /* Z */
+    FL_ZRO = 1 << 1, /* Z */        //Change the Flag codes
     FL_NEG = 1 << 2, /* N */
 };
 enum
@@ -66,14 +65,14 @@ enum
 uint16_t memory[MEMORY_MAX];  /* 65536 locations */
 uint16_t reg[R_COUNT];
 
-uint16_t sign_extend(uint16_t x, int bit_count)
+uint16_t sign_extend(uint16_t x, int bit_count)     //Basically this allows you to write anything in the 16-bit form
 {
     if ((x >> (bit_count - 1)) & 1) {
-        x |= (0xFFFF << bit_count);
-    }
+        x |= (0xFFFF << bit_count);           
+    }       
     return x;
 }
-uint16_t swap16(uint16_t x)
+uint16_t swap16(uint16_t x)     //Change the MSB to the required format
 {
     return (x << 8) | (x >> 8);
 }
@@ -92,7 +91,7 @@ void update_flags(uint16_t r)
         reg[R_COND] = FL_POS;
     }
 }
-void read_image_file(FILE* file)
+void read_image_file(FILE* file)        //Reads the particular image/obj file
 {
     /* the origin tells us where in memory to place the image */
     uint16_t origin;
@@ -113,6 +112,7 @@ void read_image_file(FILE* file)
 }
 int read_image(const char* image_path)
 {
+    printf("Image File read\n");
     FILE* file = fopen(image_path, "rb");
     if (!file) { return 0; };
     read_image_file(file);
@@ -145,7 +145,6 @@ uint16_t mem_read(uint16_t address)
     return memory[address];
 }
 DWORD fdwMode, fdwOldMode;
-
 void disable_input_buffering()
 {
     hStdin = GetStdHandle(STD_INPUT_HANDLE);
@@ -157,7 +156,6 @@ void disable_input_buffering()
     SetConsoleMode(hStdin, fdwMode); /* set new mode */
     FlushConsoleInputBuffer(hStdin); /* clear buffer */
 }
-
 void restore_input_buffering()
 {
     SetConsoleMode(hStdin, fdwOldMode);
@@ -168,10 +166,9 @@ void handle_interrupt(int signal)
     printf("\n");
     exit(-2);
 }
-
-
 int main(int argc, const char* argv[])
 {
+    printf("Before if\n");
     if (argc < 2)
     {
         /* show usage string */
@@ -181,6 +178,7 @@ int main(int argc, const char* argv[])
     
     for (int j = 1; j < argc; ++j)
     {
+        printf("In for loop\n");
         if (!read_image(argv[j]))
         {
             printf("failed to load image: %s\n", argv[j]);
@@ -201,11 +199,13 @@ int main(int argc, const char* argv[])
     int running = 1;
     while (running)
     {
+        printf("Inside While\n");
         /* FETCH */
-        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t instr = mem_read(reg[R_PC]++);     //Highlight check for incrementation
         uint16_t op = instr >> 12;
-
-        switch (op)
+        printf("op=%d\n",op);
+        printf("instr=%d\n",instr);
+        switch (op)                                 
         {
             case OP_ADD:
                 {
